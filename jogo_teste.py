0789a6de490a4e81587d851401765f9214cfa258
Pedro.Marçal <62404639+Mousegamertank@users.noreply.github.com>
2021-01-04 17:47:33 -0300

    Fim_do_primeiro_dia_aprendi_a_colocar_movimentos_e_testar_teclas

--- jogo_teste.py
+++ jogo_teste.py
@@ -9,33 +9,60 @@ pygame.init()
 #VARIAVEIS
 font = pygame.font.SysFont(Textos.fonte1.Estilo, Textos.fonte1.Tamanho)
 
+#Pegar personagem
+X = obp.p1.X
+Y = obp.p1.Y
+HEIGHT = obp.p1.heigth
+WIDTH = obp.p1.width
+
 #GERAR TELA
 def windown():
     screen = pygame.display.set_mode((800, 800),0,32)
     pygame.display.set_caption('Teste1')
 
     screen.fill([150, 200, 180])
-    pygame.draw.line(screen, cores.Cores['CIAN'], [10, 100], [630, 100], 5)
-    pygame.draw.rect(screen, cores.Cores['RED'], [obp.p1.X, obp.p1.Y, obp.p1.heigth, obp.p1.width])
+    #pygame.draw.line(screen, cores.Cores['CIAN'], [10, 100], [630, 100], 5)
+    pygame.draw.rect(screen, cores.Cores['RED'], [X, Y, HEIGHT, WIDTH])
 
     text = font.render(Textos.Textos['Teste'], True, cores.Cores['BLACK'])
 
-    screen.blit(text, [350, 200])
+    #screen.blit(text, [350, 200])
     
-    pygame.display.flip()
-
-    time.sleep(5)
-    
-
+    pygame.display.update()
+  
 run = True
 while run == True:
-    '''
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    '''
+    pygame.time.delay(100)
 
+    event = pygame.event.poll()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    Key = pygame.key.get_pressed()
+
+    if Key[pygame.K_LEFT]:
+        X -= obp.p1.velX
+        if X < 0:
+            X = 800
+    
+    if Key[pygame.K_RIGHT]:
+        X += obp.p1.velX
+        if X > 800:
+            X = 0
+        
+
+    if Key[pygame.K_UP]:
+        Y -= obp.p1.velY
+        if Y < 0:
+            Y = 800
+
+    if Key[pygame.K_DOWN]:
+        Y += obp.p1.velY
+        if Y > 800:
+            Y = 0
+        
     windown()
     
 pygame.exit()
--- objetos_e_personagens.py
+++ objetos_e_personagens.py
@@ -1,9 +1,11 @@
 class personagem():
-    def __init__(self, X, Y, heigth, width, color):
+    def __init__(self, X, Y, heigth, width, color, velX, velY):
         self.X = X
         self.Y = Y
         self.heigth = heigth
         self.width = width
         self.color = color
+        self.velX = velX
+        self.velY = velY
 
-p1 = personagem(400, 400, 20, 20, '0, 250, 200')
+p1 = personagem(200, 200, 20, 20, '0, 250, 200', 10, 10)
